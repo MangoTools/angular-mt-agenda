ced2f0c41c1ecdca4ceca3810bb21992548ce087
adidoo <adrien@mango.tools>
2014-12-05 11:14:52 +0100

    add "agenda:update" event listened on rootScope

--- angular-mt-agenda.js
+++ angular-mt-agenda.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angular.mt.agenda', [])
-    .directive('mtAgenda', [ function() {
+    .directive('mtAgenda', [ '$rootScope', function($rootScope) {
         return {
             restrict: 'AE',
             scope: {
